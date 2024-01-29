e210017b8a955544885e0ed1740ee1a81a22b42b
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-01-29 12:46:32 +0100

    form

--- ListaDeColeReact/src/App.jsx
+++ ListaDeColeReact/src/App.jsx
@@ -15,6 +15,37 @@ function App() {
     <button className="buttonnav">Eliminar</button>
     <img classname="prefer" src={Prefer} alt="prefer" />
     </nav>
+    <main>
+    <div>
+      <label className="textform">Lista:</label>
+        <input type="text" className="form-control" id="name" name="name" required/>
+    </div>
+    <form className="formlist">
+      <div className="form-group">
+      <label className="textform">Nombre:</label>
+        <input type="text" className="form-control" id="name" name="name" required/>
+    </div>
+    <div className="form-group">
+      <label className="textform">Apellido1:</label>
+        <input type="text" className="form-control" id="name" name="name" required/>
+    </div>
+    <div className="form-group">
+      <label className="textform">Apellido2:</label>
+        <input type="text" className="form-control" id="name" name="name" required/>
+    </div>
+    <div className="form-group">
+      <label className="textform">Email contacto:</label>
+        <input type="text" className="form-control" id="name" name="name" required/>
+    </div>
+    <div className="form-group">
+      <label className="textform">Teléfono:</label>
+        <input type="text" className="form-control" id="name" name="name" required/>
+    </div>
+
+    <button className="buttonform">Eliminar</button>
+
+    </form>
+   </main>
 
   
     </>
