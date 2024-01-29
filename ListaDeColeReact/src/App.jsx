6380464d44fba1539ee3819a0ebeedad5934efef
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-01-29 22:09:45 +0100

    formresult

--- ListaDeColeReact/src/App.jsx
+++ ListaDeColeReact/src/App.jsx
@@ -14,15 +14,26 @@ const UserList = () => {
     telephone: ''
   });
   const [userList, setUserList] = useState([]);
+  const [listName, setListName] = useState('');
 
   function handleNameChange(e) {
     setUser({ ...user, [e.target.name]: e.target.value })
   };
-
-  function handleAddUserToList() {
-    setUserList(prevUserList => [...prevUserList, user])
-    setUser('');
+  function handleListNameChange(e) {
+    setListName(e.target.value);
   }
+  function handleAddUserToList() {
+    setUserList(prevUserList => [...prevUserList, { ...user, listName }])
+    setUser({
+      userName: '',
+      lastName1: '',
+      lastName2: '',
+      email: '',
+      telephone: '',
+      
+    });
+  };
+
 
   console.log(UserList);
 
@@ -37,45 +48,49 @@ const UserList = () => {
       </nav>
       <main>
         <div>
-          <label className="textform">Lista:</label>
-          <input type="text" className="form-control"  required />
+          <label className="textform" name="list">Lista:</label>
+          <input type="text" className="form-control"value={listName} onChange={handleListNameChange} required />
         </div>
+        <div className="content">
         <form className="formlist">
           <div className="form-group">
             <label htmlFor="userName" className="textform">Nombre:</label>
-            <input type="text" className="form-control"  name="userName" value={user.userName} onChange={handleNameChange} required />
+            <input type="text" className="form-control" name="userName" value={user.userName} onChange={handleNameChange} required />
           </div>
           <div className="form-group">
-            <label htmlFor="lastname1"className="textform">Apellido1:</label>
-            <input type="text" className="form-control"  name="lastName1" value={user.lastName1} onChange={handleNameChange} required />
+            <label htmlFor="lastname1" className="textform">Apellido1:</label>
+            <input type="text" className="form-control" name="lastName1" value={user.lastName1} onChange={handleNameChange} required />
           </div>
           <div className="form-group">
-            <label htmlFor="lastname2"className="textform">Apellido2:</label>
-            <input type="text" className="form-control"  name="lastName2" value={user.lastName2} onChange={handleNameChange} required />
+            <label htmlFor="lastname2" className="textform">Apellido2:</label>
+            <input type="text" className="form-control" name="lastName2" value={user.lastName2} onChange={handleNameChange} required />
           </div>
           <div className="form-group">
-            <label htmlFor="email"className="textform">Email contacto:</label>
-            <input type="text" className="form-control"  name="email" value={user.email} onChange={handleNameChange} required />
+            <label htmlFor="email" className="textform">Email contacto:</label>
+            <input type="text" className="form-control" name="email" value={user.email} onChange={handleNameChange} required />
           </div>
           <div className="form-group">
-            <label htmlFor="telephone"className="textform">Teléfono:</label>
-            <input type="text" className="form-control"  name="telephone" value={user.telephone} onChange={handleNameChange} required />
+            <label htmlFor="telephone" className="textform">Teléfono:</label>
+            <input type="text" className="form-control" name="telephone" value={user.telephone} onChange={handleNameChange} required />
           </div>
 
           <button className="buttonform" onClick={handleAddUserToList}>Guardar</button>
 
         </form>
+        <form className="formresult">
+        <h2 className="list">Lista: {listName}</h2>
+          <ul>
+            {
+              userList.map((user, index) => (
+                <li key={index}>
+                  {user.userName}, {user.lastName1}, {user.lastName2}, {user.email}, {user.telephone}
+                </li>
+              ))
+            }
+          </ul>
+        </form>
+       </div>
       </main>
-
-      <ul>
-        {
-          userList.map((user, index) => (
-            <li key={index}>
-              Nombre: {user.userName}, Apellido1: {user.lastname1}, Apellido2: {user.lastname2}, Email: {user.email}, Teléfono: {user.telephone}
-            </li>
-          ))
-        }
-      </ul>
     </>
   );
 }
