c5776103a18ba28061c0e5f92ca8f9ef0f958080
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-01-29 15:07:25 +0100

    function

--- ListaDeColeReact/src/App.jsx
+++ ListaDeColeReact/src/App.jsx
@@ -1,55 +1,83 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './assets/img/logo.png'
 import Prefer from './assets/img/Prefer.svg'
 import './app.css'
 
 
-function App() {
+const UserList = () => {
+
+  const [user, setUser] = useState({
+    userName: '',
+    lastName1: '',
+    lastName2: '',
+    email: '',
+    telephone: ''
+  });
+  const [userList, setUserList] = useState([]);
+
+  function handleNameChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  };
+
+  function handleAddUserToList() {
+    setUserList(prevUserList => [...prevUserList, user])
+    setUser('');
+  }
+
+  console.log(UserList);
 
   return (
     <>
-    <nav>
-    <img className="logo" src={logo} alt="logo" />
-    <button className="buttonnav">+Lista</button>
-    <button className="buttonnav">Editar</button>
-    <button className="buttonnav">Eliminar</button>
-    <img classname="prefer" src={Prefer} alt="prefer" />
-    </nav>
-    <main>
-    <div>
-      <label className="textform">Lista:</label>
-        <input type="text" className="form-control" id="name" name="name" required/>
-    </div>
-    <form className="formlist">
-      <div className="form-group">
-      <label className="textform">Nombre:</label>
-        <input type="text" className="form-control" id="name" name="name" required/>
-    </div>
-    <div className="form-group">
-      <label className="textform">Apellido1:</label>
-        <input type="text" className="form-control" id="name" name="name" required/>
-    </div>
-    <div className="form-group">
-      <label className="textform">Apellido2:</label>
-        <input type="text" className="form-control" id="name" name="name" required/>
-    </div>
-    <div className="form-group">
-      <label className="textform">Email contacto:</label>
-        <input type="text" className="form-control" id="name" name="name" required/>
-    </div>
-    <div className="form-group">
-      <label className="textform">Teléfono:</label>
-        <input type="text" className="form-control" id="name" name="name" required/>
-    </div>
+      <nav>
+        <img className="logo" src={logo} alt="logo" />
+        <button className="buttonnav">+Lista</button>
+        <button className="buttonnav">Editar</button>
+        <button className="buttonnav">Eliminar</button>
+        <img className="prefer" src={Prefer} alt="prefer" />
+      </nav>
+      <main>
+        <div>
+          <label className="textform">Lista:</label>
+          <input type="text" className="form-control"  required />
+        </div>
+        <form className="formlist">
+          <div className="form-group">
+            <label htmlFor="userName" className="textform">Nombre:</label>
+            <input type="text" className="form-control"  name="userName" value={user.userName} onChange={handleNameChange} required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastname1"className="textform">Apellido1:</label>
+            <input type="text" className="form-control"  name="lastName1" value={user.lastName1} onChange={handleNameChange} required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastname2"className="textform">Apellido2:</label>
+            <input type="text" className="form-control"  name="lastName2" value={user.lastName2} onChange={handleNameChange} required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email"className="textform">Email contacto:</label>
+            <input type="text" className="form-control"  name="email" value={user.email} onChange={handleNameChange} required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="telephone"className="textform">Teléfono:</label>
+            <input type="text" className="form-control"  name="telephone" value={user.telephone} onChange={handleNameChange} required />
+          </div>
 
-    <button className="buttonform">Eliminar</button>
+          <button className="buttonform" onClick={handleAddUserToList}>Guardar</button>
 
-    </form>
-   </main>
+        </form>
+      </main>
 
-  
+      <ul>
+        {
+          userList.map((user, index) => (
+            <li key={index}>
+              Nombre: {user.userName}, Apellido1: {user.lastname1}, Apellido2: {user.lastname2}, Email: {user.email}, Teléfono: {user.telephone}
+            </li>
+          ))
+        }
+      </ul>
     </>
   );
 }
 
-export default App;
+export default UserList;
